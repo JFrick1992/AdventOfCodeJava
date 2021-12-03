0b82faf429ebcda1fd3d8b62c65818dda3bf1d67
Jacob Frick <jacobhfrick1992@gmail.com>
2021-12-02 22:16:57 -0800

    added day one problem two code

--- src/DayOne/ProblemTwo.java
+++ src/DayOne/ProblemTwo.java
@@ -1,4 +1,21 @@
 package DayOne;
 
+import utils.ReadFile;
+import java.util.ArrayList;
+
 public class ProblemTwo {
+    public static final String FILE_NAME = "input.txt";
+    public static void runProblemTwo() {
+        int count = 0;
+        ArrayList<Integer> depths = ReadFile.getFileAsIntegerList(ProblemOne.FILE_NAME);
+        System.out.println("1.2 is to determine how many times the sub's depth INCREASED over window SIZE = 3");
+        System.out.println("Running 1.2:");
+        for (int i = 0; i < depths.size() -3; i++) {
+
+            if((depths.get(i) + depths.get(i + 1) + depths.get(i + 2)) < (depths.get(i+1) + depths.get(i + 2) + depths.get(i + 3))) {
+                count++;
+            }
+        }
+        System.out.println("The total number of INCREASES were: " + count);
+    }
 }
